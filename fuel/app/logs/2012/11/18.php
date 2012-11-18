626976aea261437e638f06533f7657ca9a340e2b
JKANEKI <JKANEKI@JKANEKI-PC>
2012-11-18 18:09:43 +0900

    新規作成（テスト用）

---
+++ fuel/app/logs/2012/11/18.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-11-18 08:02:14 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-11-18 08:19:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
